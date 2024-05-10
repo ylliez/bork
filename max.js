e08df66efd9a30f55b8fc5de912e439099a60d5b
ylliez <illiez@hotmail.com>
2024-05-10 14:32:45 -0500

    Update max.js

--- max.js
+++ max.js
@@ -3,6 +3,6 @@ const Max = require('max-api-or-not');
 Max.post("Max/MSP API loaded");
 // import Socket.IO client library & set socket namespace
 const io = require("socket.io-client");
-const socket = io("https://nodechestra.herokuapp.com/max");
+const socket = io("https://bork-7250e811ddfb.herokuapp.com/max");
 socket.on("connect", () => { Max.post(`client ID: ${socket.id}`); });
 socket.onAny((args) => { Max.outlet(args); });
